097493111466f51b6211a98dcc11e64f7886c9cc
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-25 21:04:18 -0300

    Alterar status dinamicamente ao clicar no checkbox.

--- script.js
+++ script.js
@@ -2,28 +2,58 @@ const tarefas = [];
 
 const nometarefa = document.getElementById("nomeTarefa");
 const prioridade = document.getElementById("prioridade");
+const buscatarefa = document.getElementById("buscaTarefa");
 const catalogoTarefas = document.getElementById("catalogoTarefas");
 const botaoSubmeter = document.getElementById("botaoSubmeter");
 
+function criarTarefas() {
+  if (!nometarefa.value.trim()) return;
 
-function criarTarefas(){
-    tarefas.push({titulo: nometarefa.value, prioridade: prioridade.value});
-    console.log(tarefas);
-    exibirTarefas();
+  tarefas.push({ titulo: nometarefa.value, prioridade: prioridade.value,
+    status: "Tarefa em andamento"});
+  
+  nometarefa.value = "";
+  prioridade.value = "";
+
+  exibirTarefas();
 }
 
-function exibirTarefas(){
-  tarefas.forEach(tarefa => {
+function exibirTarefas() {
+  catalogoTarefas.innerHTML = "";
 
-    catalogoTarefas.innerHTML = ``;
-
+  tarefas.forEach((tarefa, index) => {
     const lista = document.createElement("li");
-    lista.innerHTML = `<p>Nome da tarefa: ${tarefa.titulo}</p>
-    <p>Prioridade da tarefa: ${tarefa.prioridade}</p>
-    <form><input type="checkbox"></form>`;
+    const campoStatus = tarefa.status === "Concluída" 
+      ? `<p class="verificar">"${tarefa.status}"</p>` 
+      : `<p class="verificar"><input type="checkbox" class="check"> ${tarefa.status}</p>`;
+    
+    lista.classList.add("lista");
+    lista.innerHTML = `
+      <p>Nome da tarefa: ${tarefa.titulo}</p>
+      <p>Prioridade da tarefa: ${tarefa.prioridade}</p>
+      ${campoStatus}
+    `;
 
-    catalogoTarefas.append(lista);
-  })
+    const check = lista.querySelector(".check");
+
+    if (check) {
+      check.addEventListener("change", () => {
+        if (check.checked) {
+          setTimeout(() => {
+            tarefa.status = "Concluída";
+            exibirTarefas();
+          }, 1000);
+        }
+      });
+    }
+
+    catalogoTarefas.appendChild(lista);
+  });
 }
+
+function buscaTarefa(){
+  Object.values(tarefas).includes(buscaTarefa.value);
+}
+
 
 botaoSubmeter.addEventListener("click", criarTarefas);
